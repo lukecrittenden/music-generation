7eb2b0b0deb0ea9f554bc795a2d87f257840ea4c
Luke Crittenden <luke.crittenden@icloud.com>
2025-01-13 20:38:07 +0000

    Add graph-traversal-based example and update README

---
+++ README.md
@@ -0,0 +1,7 @@
+## Examples of Approaches to Computer-Based Music Generation
+
+### Simple rule-based system
+This example showcases an algorithmic rule-based system for generating piano melodies with harmonisation in 4/4 time, where the output can be in any major or minor key. This is a very simple, straightforward approach to rule-based music generation.
+
+### Graph-traversal-based system
+This example makes use of the graph traversal method for generating chord progressions.
---
+++ graph-traversal-based/main.py
@@ -0,0 +1,59 @@
+from midiutil import MIDIFile
+import random
+from typing import List, Tuple, Dict
+
+TRACK = 0
+CHANNEL = 0
+TEMPO = 120
+VOLUME = 90
+
+NOTE_DURATION = 2
+
+def OutputMIDI(chords: 'Chords', filename: str) -> None:
+    try:
+        MIDIMelody = MIDIFile(1)
+        MIDIMelody.addTempo(TRACK, 0, TEMPO)
+        for note in chords.chords:
+            MIDIMelody.addNote(TRACK, CHANNEL, note[0], note[1], note[2], VOLUME)
+        with open(filename + ".mid", "wb") as outputFile:
+            MIDIMelody.writeFile(outputFile)
+    except:
+        print("An error occurred while generating the MIDI file")
+
+class Chords():
+    def __init__(self, graph: Dict[Tuple[int, int, int], List[Tuple[int, int, int]]]) -> None:
+        self.chords: List[Tuple[int, int, int]] = []
+        self.graph = graph
+
+    def generateChords(self, totalChords: int) -> None:
+        time = 0
+
+        currentChord = random.choice(list(self.graph.keys()))
+        self.chords.extend([(currentChord[0], time, NOTE_DURATION), (currentChord[1], time, NOTE_DURATION), (currentChord[2], time, NOTE_DURATION)])
+
+        while len(self.chords) // 3 < totalChords and currentChord in self.graph and self.graph[currentChord]:
+            nextChord = random.choice(self.graph[currentChord])
+            time += NOTE_DURATION
+            self.chords.extend([(nextChord[0], time, NOTE_DURATION), (nextChord[1], time, NOTE_DURATION), (nextChord[2], time, NOTE_DURATION)])
+            currentChord = nextChord
+
+if __name__ == '__main__':
+    graph = {
+        (60, 64, 67): [(65, 69, 72), (67, 71, 74), (62, 65, 69), (64, 67, 71)],
+        (65, 69, 72): [(67, 71, 74), (60, 64, 67), (69, 72, 76), (62, 65, 69)],
+        (67, 71, 74): [(60, 64, 67), (65, 69, 72), (69, 72, 76), (64, 67, 71)],
+        (62, 65, 69): [(60, 64, 67), (65, 69, 72), (67, 71, 74), (64, 67, 71)],
+        (64, 67, 71): [(60, 64, 67), (65, 69, 72), (67, 71, 74), (62, 65, 69)],
+        (69, 72, 76): [(60, 64, 67), (65, 69, 72), (67, 71, 74), (64, 67, 71)],
+        (60, 63, 67): [(65, 68, 72), (67, 70, 74), (62, 65, 68), (64, 67, 70)],
+        (65, 68, 72): [(67, 70, 74), (60, 63, 67), (69, 72, 75), (62, 65, 68)],
+        (67, 70, 74): [(60, 63, 67), (65, 68, 72), (69, 72, 75), (64, 67, 70)],
+        (62, 65, 68): [(60, 63, 67), (65, 68, 72), (67, 70, 74), (64, 67, 70)],
+        (64, 67, 70): [(60, 63, 67), (65, 68, 72), (67, 70, 74), (62, 65, 68)],
+        (69, 72, 75): [(60, 63, 67), (65, 68, 72), (67, 70, 74), (64, 67, 70)],
+    }
+
+    chordProgression = Chords(graph)
+    chordProgression.generateChords(4)
+
+    OutputMIDI(chordProgression, "Untitled")
